1f4701fcb8f46ce6fb8df7db033ad5e40a0ff1ba
wmruben22 <116590587+wmruben22@users.noreply.github.com>
2023-08-30 15:48:25 -0400

    Add files via upload

---
+++ YouDoIt3/TestInfo.java
@@ -0,0 +1,10 @@
+//Bill Ruben
+// p. 90
+public class TestInfo
+{
+ public static void main(String[] args)
+ {
+  System.out.println("Calling method from another class:");
+  ParadiseInfo.displayInfo();
+ }
+}
